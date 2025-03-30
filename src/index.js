db35ab819bd3adf3c40ea7d366bd4c899147afbe
jdmpintado <137705155+jdmpintado@users.noreply.github.com>
2025-03-30 19:50:40 +0100

    Added code to inject icon according to city searched

--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
@@ -35,6 +36,7 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   timeElement.innerHTML = formatDate(date);
+  iconElement.innerHTML = `<img src = "${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
 function searchCity(city) {
